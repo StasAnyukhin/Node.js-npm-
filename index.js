7d6c435cf7befdac6929d0634d11452d084a8509
Stas.Anyukhin <penza8028@gmail.com>
2024-11-06 03:54:15 +0300

    Домашнее задание №8

---
+++ index.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+axios.get('https://api.example.com/data')
+  .then(response => console.log(response.data))
+  .catch(error => console.error('Error:', error));
+// console.log("Hello from Parcel!");
---
+++ server.js
@@ -0,0 +1,3 @@
+const math = require('./math');
+console.log(math.add(19, 6));
+console.log(math.subtract(15, 8));
