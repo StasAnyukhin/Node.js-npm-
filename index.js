ce14f603fbb3aa4425d328d0ef74fa0d00756b75
Stas.Anyukhin <penza8028@gmail.com>
2024-11-08 01:18:42 +0300

    добавление папки src b bcghfdktybt ytljx jd

--- index.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios');
-axios.get('https://api.example.com/data')
-  .then(response => console.log(response.data))
-  .catch(error => console.error('Error:', error));
-// console.log("Hello from Parcel!");
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const axios = require("axios");
+axios
+  .get("https://api.example.com/data")
+  .then((response) => console.log(response.data))
+  .catch((error) => console.error("Error:", error));
+
+console.log("Hello from Parcel!");
